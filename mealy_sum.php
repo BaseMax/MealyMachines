98b88213e7a64cc2d6bbed6b418cb0ef9b104bcf
Max Base <maxbasecode@gmail.com>
2023-01-31 17:05:10 +0000

    Fix bug

--- mealy_sum.php
+++ mealy_sum.php
@@ -2,18 +2,22 @@
 function mealy_sum($inputs) {
     $state = "start";
     $sum = 0;
+    $num = 0;
     foreach (str_split($inputs) as $i) {
         if ($state == "start") {
             if ($i == "+") {
                 $state = "add";
+                $sum += $num;
+                $num = 0;
             } else {
-                $sum = $sum * 10 + intval($i);
+                $num = $num * 10 + intval($i);
             }
         } elseif ($state == "add") {
-            $sum += intval($i);
+            $num = intval($i);
             $state = "start";
         }
     }
+    $sum += $num;
     return $sum;
 }
 
